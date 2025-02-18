8a6b758d18973e2418bed3bc8272baf6289a674f
Casim <Casim@Surface11>
2025-02-18 11:29:25 +0000

    Adding a tester file to ensure that the repo is working

---
+++ CompressorShelf/PolytropicEfficiency.m
@@ -0,0 +1,8 @@
+function [outputArg1,outputArg2] = PolytropicEfficiency(inputArg1,inputArg2)
+%POLYTROPICEFFICIENCY Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+ % this is meant to be a test to ensure that everything works
+end
+
